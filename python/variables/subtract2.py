152804fecd7e41226f84df08967d334016d02c89
Megan Leich <51799170+maleich@users.noreply.github.com>
2020-11-17 21:02:20 -0800

    add subtract2

---
+++ python/variables/subtract2.py
@@ -0,0 +1,15 @@
+# @desc Determine the value returned by the function.
+
+def minus(num):
+    diff = num - 6.5
+    return diff
+
+
+def main():
+    print(minus(5))
+    print(minus(6.5))
+    print(minus(10))
+
+
+if __name__ == '__main__':
+    main()
